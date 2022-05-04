ffeee2a738c63b2b835d02236360a858908b62f3
Viktor <vandonov@estafet.com>
2022-05-04 15:15:47 +0300

    adding the beggining

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,5 +3,6 @@ package org.example;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("This is just the beginning");
   }
 }
